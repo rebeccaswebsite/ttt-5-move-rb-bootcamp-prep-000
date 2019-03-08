f9bf38efaf36e229464b1381aea5c97646f2be60
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-03-08 12:56:44 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end  
 
-def move(board, index, character)
+def move(board, index, character = "X")
   board[index] = character
-  return board
 end
